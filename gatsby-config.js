361fbc139d93da67eaf83c81d4cad9aa53f0668e
Marina Kampisiou <kampisioum@kotsovolos.gr>
2025-09-08 15:35:13 +0300

    changed packageManager to yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,21 +13,5 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `static/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
   ],
 }
